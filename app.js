082cce12daf9ff8b172bfe8c5a4c0a494b66dde4
Paul Thomas <paulthomas2107@gmail.com>
2022-12-19 13:07:35 +0000

    More...

--- app.js
+++ app.js
@@ -49,27 +49,36 @@ class Flower {
     this.x = x;
     this.y = y;
     this.size = size;
-    this.maxFlowerSize = this.size + Math.random() * 50;
+    this.vs = Math.random() * 0.3 + 0.2;
+    this.maxFlowerSize = this.size + Math.random() * 180;
     this.image = new Image();
     this.image.src = 'flowers.png';
     this.frameSize = 100;
     this.frameX = Math.floor(Math.random() * 3);
     this.frameY = Math.floor(Math.random() * 3);
+    this.size > 11.5 ? (this.willFlower = true) : (this.willFlower = false);
+    this.angle = 0;
+    this.va = Math.random() * 1 - 0.5;
   }
   grow() {
-    if (this.size < this.maxFlowerSize) {
-      this.size += 0.3;
+    if (this.size < this.maxFlowerSize && this.willFlower) {
+      this.size += this.vs;
+      this.angle += this.va;
+      ctx.save();
+      ctx.translate(this.x, this.y);
+      ctx.rotate(this.angle);
       ctx.drawImage(
         this.image,
         this.frameSize * this.frameX,
         this.frameSize * this.frameY,
         this.frameSize,
         this.frameSize,
-        this.x - this.size / 2,
-        this.y - this.size / 2,
+        0 - this.size / 2,
+        0 - this.size / 2,
         this.size,
         this.size
       );
+      ctx.restore();
       requestAnimationFrame(this.grow.bind(this));
     }
   }
